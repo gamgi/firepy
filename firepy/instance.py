55f5d5b4588b932077e008c1904cc88025ba2d37
Pietu R <pietu_r@gmx.com>
2021-07-06 19:27:31 +0300

    move error handling to decorator

--- firepy/instance.py
+++ firepy/instance.py
@@ -1,19 +1,21 @@
+from functools import wraps
 from io import StringIO
 from requests.exceptions import ConnectionError, HTTPError
 from firepy.connection import Connection
 from firepy.exceptions import err_from_stderr, FirecrackerApiError
 
 
-class Instance:
-    conn: Connection
+def handle_errors(func):
+    """Decorator that humanizes exceptions.
 
-    def __init__(self, socket_path: str, stderr: StringIO = None):
-        self.conn = Connection(socket_path)
-        self.stderr = stderr
-
-    def _put(self, path: str, **kwargs):
+    It
+    - parses firecracker HTTP responses
+    - checks instance stderr for error messages
+    """
+    @wraps(func)
+    def wrapper(self: 'Instance', *args, **kwargs):
         try:
-            self.conn.put(path, **kwargs)
+            func(self, *args, **kwargs)
         except HTTPError as err:
             res = err.response.json()
             if 'fault_message' in res:
@@ -24,18 +26,36 @@ class Instance:
                 raise err_from_stderr(self.stderr) from err
             else:
                 raise
+    return wrapper
 
+
+class Instance:
+    conn: Connection
+
+    def __init__(self, socket_path: str, stderr: StringIO = None):
+        self.conn = Connection(socket_path)
+        self.stderr = stderr
+        # TODO check socket exists
+
+    @handle_errors
     def start(self):
-        self._put('/actions', json={'action_type': 'InstanceStart'})
+        self.conn.put('/actions', json={'action_type': 'InstanceStart'})
 
+    @handle_errors
     def pause(self):
-        pass
+        self.conn.put('/vm', json={'state': 'Paused'})
 
+    @handle_errors
+    def resume(self):
+        self.conn.put('/vm', json={'state': 'Resumed'})
+
+    @handle_errors
     def set_config(self, **kwargs):
-        self._put('/machine-config', json={
+        self.conn.put('/machine-config', json={
             **kwargs
         })
 
+    @handle_errors
     def set_kernel(self, kernel_path: str, **kwargs):
         self.conn.put('/boot-source', json={
             "kernel_image_path": kernel_path,
@@ -43,6 +63,7 @@ class Instance:
             **kwargs
         })
 
+    @handle_errors
     def set_rootfs(self, rootfs_path: str, **kwargs):
         self.conn.put('/drives/rootfs', json={
             "drive_id": "rootfs",
