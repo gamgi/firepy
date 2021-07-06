0e99519f59c3e03b0e6ea3b18141f478930165a6
Pietu R <pietu_r@gmx.com>
2021-07-06 21:43:52 +0300

    simplify vm ids

--- firepy/firecracker.py
+++ firepy/firecracker.py
@@ -1,28 +1,35 @@
 import re
 from io import StringIO
 from pathlib import Path
-from uuid import uuid4
 from sh import Command, ErrorReturnCode, TimeoutException
 from firepy.instance import Instance
 from firepy.exceptions import err_from_returncode
 
 
 REGEX_STDERR_MESSAGE = re.compile('.*? \\[[\\w -:]+\\] (.*)', re.MULTILINE)
+DEFAULT_FIRECRACKER = Command('./firecracker')
 
 
 class Firecracker:
     vms: list[Instance] = []
     run: Command
+    _id: int = 0
 
-    def __init__(self, run_command: Command = Command('./firecracker')):
-        self.run = run_command
+    def __init__(self, command: Command = None):
+        self.run = command or DEFAULT_FIRECRACKER
 
-    def _create_socket(self, override: str = None) -> str:
-        socket_path = override or f'/tmp/firecracker-{uuid4()}.socket'
-        Path(socket_path).unlink(missing_ok=True)
-        return socket_path
+    def _next_free_id(self) -> int:
+        # TODO: reuse ids
+        next_id = self._id
+        self._id += 1
+        return next_id
 
-    def _create_instance(self, socket_path, sleep=2) -> Instance:
+    def _create_socket(self, id: int) -> str:
+        path = f'/tmp/firecracker-{id}.socket'
+        Path(path).unlink(missing_ok=True)
+        return path
+
+    def _create_instance(self, socket_path: str = None, sleep=2) -> Instance:
         stderr = StringIO()
         try:
             self.run("--api-sock", socket_path,
@@ -34,8 +41,9 @@ class Firecracker:
         return vm
 
     def create_vm(self, jailed=True, socket_path: str = None) -> Instance:
-        _socket_path = self._create_socket(socket_path)
+        vm_id = self._next_free_id()
+        vm_socket_path = socket_path or self._create_socket(vm_id)
         try:
-            return self._create_instance(_socket_path)
+            return self._create_instance(vm_socket_path, vm_id)
         except ErrorReturnCode as err:
             raise err_from_returncode(err) from err
--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,14 +1,5 @@
-import pytest
-from unittest.mock import Mock, patch, ANY
+from unittest.mock import Mock, ANY
 from firepy.firecracker import Firecracker
-
-MOCK_IDS = ['id1', 'id2', 'id3']
-
-
-@pytest.fixture(autouse=True)
-def mock_uuid():
-    with patch('firepy.firecracker.uuid4', side_effect=MOCK_IDS):
-        yield
 
 
 class TestE2E:
@@ -17,13 +8,13 @@ class TestE2E:
         Firecracker(binary).create_vm()
 
         binary.assert_called_with(
-            '--api-sock', '/tmp/firecracker-id1.socket',
+            '--api-sock', '/tmp/firecracker-0.socket',
             **{'_bg': True, '_err': ANY}
         )
 
     def test_start_vm_calls_firecracker_api(self, requests_mock):
         m = requests_mock.put(
-            'http+unix://%2Ftmp%2Ffirecracker-id1.socket/actions')
+            'http+unix://%2Ftmp%2Ffirecracker-0.socket/actions')
 
         fc = Firecracker(Mock())
         fc.create_vm().start()
